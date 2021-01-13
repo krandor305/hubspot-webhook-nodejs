fcee4731f6d84bd6ede801de347c7dd06acf241f
alihaddani <al-had@hotmail.com>
2021-01-13 14:52:13 +0100

    body push

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ http.createServer((request, response) => {
   request.on('data', (chunk) => {
     body.push(chunk);
   }).on('end', (req,res) => {
+    console.log(req);
     console.log(res);
     //body = Buffer.concat(body).toString();
     const appSecret = process.env.APP_SECRET;
